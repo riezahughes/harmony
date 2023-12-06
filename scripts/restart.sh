c2d93f2da700e856dd83dc498f6b3c74997f676c
riezahughes <dan@tantalus-troupe.com>
2023-12-06 21:50:01 +0100

    deployment changes

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -5,6 +5,8 @@ servicename="healthbot"
 
 
 if systemctl --all --type service | grep -q "$servicename";then
+    bun x prisma migrate deploy
+    bun x prisma generate
     service $servicename stop
     systemctl daemon-reload
     service $servicename start
