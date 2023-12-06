ca26dd05a86811d844e04d789bfec4a48041cd3a
riezahughes <dan@tantalus-troupe.com>
2023-12-06 22:16:44 +0100

    trying again

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -5,8 +5,6 @@ servicename="healthbot"
 
 
 if systemctl --all --type service | grep -q "$servicename";then
-    npx prisma migrate deploy
-    npx prisma generate
     service $servicename stop
     systemctl daemon-reload
     service $servicename start
